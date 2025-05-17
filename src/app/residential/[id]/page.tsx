c192cff125e509850f3c8c323a571eb26e839d17
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2025-05-17 14:23:00 -0400

    Aff fake data

--- src/app/residential/[id]/page.tsx
+++ src/app/residential/[id]/page.tsx
@@ -10,12 +10,23 @@ interface PageProps {
 export default async function Page({ params }: PageProps) {
   //NEXTJS requires params to be a Promise or it will throw an error
   // see https://nextjs.org/docs/messages/sync-dynamic-apis
+  // linting will say this is wrong but it is correct
+  //you can use Promise.resolve to convert it to a promise
+  // const params = await Promise.resolve(params);
+
   const { id } = await params;
 
   if (!id) {
     // If the section is not found, return a 404 page
     notFound();
   }
+  const residential = [
+    { id: "123", name: "Residential 123" },
+    { id: "2", name: "Residential 2" },
+    { id: "3", name: "Residential 3" },
+  ];
+
+  const residentialProperty = residential?.find((res) => res.id === id);
 
   return (
     <div>
@@ -26,8 +37,9 @@ export default async function Page({ params }: PageProps) {
         Back to Home
       </Link>
       <div className="p-4">
-        <h2 className="text-lg font-bold">Residential ID: {id}</h2>
-        <p>Details about the residential property with ID: {id}</p>
+        <h2 className="text-lg font-bold">Residential Property Details</h2>
+        <p>ID: {residentialProperty?.id}</p>
+        <p>Name: {residentialProperty?.name}</p>
       </div>
       {/* Add more content or components here */}
     </div>
